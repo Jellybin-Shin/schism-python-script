90cd87bc2c5f91cbbf16dd6df06cba697b0def57
Dongbin Shin <jellydbshin@gmail.com>
2020-10-19 11:01:01 +0900

    update wvel in visit-scripts

---
+++ hyc-scripts/tmp/UV_vector_nc.py
@@ -0,0 +1,88 @@
+#%matplotlib inline
+import numpy as np, pandas as pd, datetime, netCDF4 as nc4, glob, os, sys
+
+'''
+        short water_v(time, depth, lat, lon) ;
+                water_v:long_name = "Northward Water Velocity" ;
+                water_v:standard_name = "northward_sea_water_velocity" ;
+                water_v:units = "m/s" ;
+                water_v:_FillValue = -30000s ;
+                water_v:missing_value = -30000s ;
+                water_v:scale_factor = 0.001f ;
+                water_v:add_offset = 0.f ;
+                water_v:NAVO_code = 18 ;
+                water_v:coordinates = "time depth lat lon lat lon" ;
+
+        float hvel(time, nSCHISM_hgrid_node, nSCHISM_vgrid_layers, two) ;
+                hvel:missing_value = 9.96921e+36f ;
+                hvel:mesh = "SCHISM_hgrid" ;
+                hvel:data_horizontal_center = "node" ;
+                hvel:data_vertical_center = "full" ;
+                hvel:i23d = 2 ;
+                hvel:ivs = 2 ;
+'''
+work_dir  = "/home/dbshin/01_WORKS/99_EXT_DATAS/HYCOM_DATA"
+os.makedirs(work_dir+'/outputs', exist_ok=True)
+file_list = glob.glob('{}/UV_*nc'.format(work_dir))
+if len(file_list) != 0 :
+    print('Reading Files List')
+else : raise Exception('There is no UV file in working directory')
+
+toexclude = ["TO_REMOVE"]
+for ii in range(0,1):
+    with nc4.Dataset(file_list[ii]) as src, nc4.Dataset('{}/outputs/{}.vec.nc'.format(work_dir,file_list[ii].split(sep='/',)[-1]), "w") as dst:
+        # copy attributes
+        for name in src.ncattrs():
+            dst.setncattr(name, src.getncattr(name))
+        # copy dimensions
+        for name, dimension in src.dimensions.items():
+            dst.createDimension(
+                name, (len(dimension) if not dimension.isunlimited else None))
+        # copy all file data except for the excluded
+        for name, variable in src.variables.items():
+            if name not in toexclude:
+                x = dst.createVariable(name, variable.datatype, variable.dimensions)
+                dst.variables[name][:] = src.variables[name][:]
+            
+# for ii in range(0,1):
+#     # Open the UV_*.nc file
+#     print('Open "{}" file.'.format(file_list[ii]))
+#     ncori = nc4.Dataset(file_list[ii],"r",format="netcdf4")
+#     ncout = nc4.Dataset('{}/outputs/{}.vec.nc'.format(work_dir,file_list[ii].split(sep='/',)[-1]),'w', format='NETCDF4') #'w' stands for write
+#     print(ncori.__dict__)
+#     ncout.setncatts(ncori.__dict__)
+#     for name, dimension in ncori.dimensions.items():
+#         ncout.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
+    
+#     for name, variable in ncori.variables.items():
+#         if name not in toexclude:
+#             x = ncout.createVariable(name, variable.datatype, variable.dimensions)
+#             ncout[name][:] = ncori[name][:]
+#             ncout[name].setncatts(ncori[name].__dict__)
+ncout.close()
+    # # Reading variables
+    # depthi = ncori.variables['depth']
+    # time = ncori.variables['time']
+    # lat= ncori.variables['lat']
+    # lon= ncori.variables['lon']
+    # uu = ncori.variables['water_u']
+    # vv = ncori.variables['water_v']
+    
+    # uf = np.array(uu[:],'f')
+    # vf = np.array(vv[:],'f')
+    # uf = np.where(uf!=uu._FillValue, uf, np.nan)
+    # vf = np.where(vf!=vv._FillValue, vf, np.nan)
+
+    
+    # # Outputs
+    # ncout.createDimension('time', None)
+    # ncout.createDimension('nx_grid', len(lon[:]))
+    # ncout.createDimension('ny_grid', len(lat[:]))
+    # ncout.createDimension('depth', len(depthi[:]))
+    # ncout.createDimension('rank', 2)
+
+    # deptho = ncout.createVariable('time','f4','time')
+
+
+    
+
---
+++ visit-scripts/01_wind_vel.py
@@ -0,0 +1,70 @@
+'''
+This script needs : 
+  - visit
+  - python 2.7
+  - numpy  
+'''
+## LIBRARIES AND LAUNCH VISIT ##
+import sys, os, numpy as np
+visit_dir = "/home/dbshin/99_libraries/visit" 
+# visit_dir = "/home/dbshin/03_utility/visit2.10.2"  server80
+sys.path.append("{}/2.10.2/linux-x86_64/lib/site-packages".format(visit_dir))
+
+from visit import *
+Launch()
+# ========  Templetes  ======== #
+elev = PseudocolorAttributes()
+elev.min, elev.minFlag = -1.0, 1
+elev.max, elev.maxFlag = 1.0, 1
+
+wvelm = PseudocolorAttributes()
+wvelm.min, wvelm.minFlag = 0.0, 1
+wvelm.max, wvelm.maxFlag = 0.5, 1
+
+zs = SliceAttributes()
+zs.originType = zs.Point
+zs.originPoint = (0,0,-5)
+zs.normal = (0,0,1)
+zs.axisType = zs.ZAxis
+
+
+wvel = VectorAttributes()
+wvel.min, wvel.minFlag = 0.0, 1
+wvel.max, wvel.maxFlag = 0.5, 1
+wvel.colorByMag = 0
+wvel.scaleByMagnitude = 1
+wvel.scale = 0.25
+wvel.useStride = 1
+wvel.stride = 1
+
+save = SaveWindowAttributes()
+save.fileName = "wind_vector"
+save.width, save.height = 1024,768
+save.screenCapture = 0
+# ============================= #
+
+# Step 1: Open a database
+OpenDatabase("../schout_*.nc database")
+
+# Step 2: Add plots
+AddPlot("Pseudocolor", "wind_speed_magnitude")
+SetPlotOptions(wvelm)
+SetOperatorOptions(zs)
+
+AddPlot("Vector", "wind_speed")
+SetPlotOptions(wvel)
+SetOperatorOptions(zs)
+
+
+# Step 3: Draw the plots
+DrawPlots()
+
+# Step 4: Animate through time and asve images
+tstride = 11
+time   = np.arange(1,TimeSliderGetNStates(),tstride)
+SetSaveWindowAttributes(save)
+for state in time:
+  SetTimeSliderState(state)
+  SaveWindow()
+
+# SetTimeSliderState(time[ii]); ii = ii +1
